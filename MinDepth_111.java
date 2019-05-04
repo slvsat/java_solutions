4c88ded0953f3b32e679face98cb47662e694116
Sattar <salambaev.sattar@gmail.com>
2019-05-04 11:53:13 +0600

    few more

---
+++ MinDepth_111.java
@@ -0,0 +1,18 @@
+public class MinDepth_111 {
+
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        return depth(root);
+    }
+
+    public int depth(TreeNode node) {
+        if (node == null) return -1;
+        int left = depth(node.left);
+        int right = depth(node.right);
+        if (left == -1 && right == -1) return 1;
+        if (left == -1 || right == -1) return Math.max(left, right) + 1;
+        return Math.min(left, right) + 1;
+    }
+
+
+}
