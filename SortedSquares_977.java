3d5ac2d9e96ea24e9616d2f2bccb89e4f8a05ef5
Sattar <salambaev.sattar@gmail.com>
2019-05-06 01:28:20 +0600

    few more before sleep

---
+++ SortedSquares_977.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class SortedSquares_977 {
+
+
+    public int[] sortedSquares(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A[i] * A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+
+}
---
+++ ToLowerCase_709.java
@@ -0,0 +1,13 @@
+public class ToLowerCase_709 {
+
+    public String toLowerCase(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i<str.length(); i++) {
+            if (str.charAt(i) >= 65 && str.charAt(i) <= 90) {
+                result.append((char)(str.charAt(i) + 32));
+            } else result.append(str.charAt(i));
+        }
+        return result.toString();
+    }
+
+}
