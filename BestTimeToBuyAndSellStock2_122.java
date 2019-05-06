1e61e8fc693890f054b706a8a8a867f93f1140d3
Sattar <salambaev.sattar@gmail.com>
2019-05-06 15:59:53 +0600

    best time to buy and sell

---
+++ BestTimeToBuyAndSellStock2_122.java
@@ -0,0 +1,25 @@
+public class BestTimeToBuyAndSellStock2_122 {
+
+    public int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int sumProfit = 0;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            }
+            else if (prices[i] > minprice){
+                if (prices[i] - minprice > maxprofit) maxprofit = prices[i] - minprice;
+                sumProfit += maxprofit;
+                maxprofit = 0;
+                minprice = prices[i];
+            }
+            else if (prices[i] - minprice > maxprofit){
+                maxprofit = prices[i] - minprice;
+            }
+        }
+        if (sumProfit > 0) return sumProfit;
+        return maxprofit;
+    }
+
+}
