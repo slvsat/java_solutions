5c0fcff7a45da638b07766fd6f6de6f223b311f0
Sattar <salambaev.sattar@gmail.com>
2019-05-06 14:32:54 +0600

    few more

---
+++ BestTimeToBuyAndSellStock_121.java
@@ -0,0 +1,36 @@
+public class BestTimeToBuyAndSellStock_121 {
+
+    public static int maxProfit(int[] prices) {
+        int minBuy = Integer.MAX_VALUE;
+        int maxSell = 0;
+        int maxProfit = 0;
+        for (int i=0; i < prices.length; i++) {
+            if (prices[i] < minBuy) {
+                for (int d = i+1; d < prices.length; d++) {
+                    if (maxSell < prices[d]) {
+                        maxSell = prices[d];
+                        minBuy = prices[i];
+                        maxProfit = Math.max(maxProfit, maxSell - minBuy);
+                    }
+                }
+            }
+            maxSell = 0;
+        }
+        if (maxProfit > 0) return maxProfit;
+        return 0;
+    }
+
+    // Better solution
+//    public int maxProfit(int[] prices) {
+//        int minprice = Integer.MAX_VALUE;
+//        int maxprofit = 0;
+//        for (int i = 0; i < prices.length; i++) {
+//            if (prices[i] < minprice)
+//                minprice = prices[i];
+//            else if (prices[i] - minprice > maxprofit)
+//                maxprofit = prices[i] - minprice;
+//        }
+//        return maxprofit;
+//    }
+
+}
