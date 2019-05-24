7c96cf9fd512c936d4766a27d04d2daac282eaf6
Sattar <salambaev.sattar@gmail.com>
2019-05-24 19:38:34 +0600

    few more

---
+++ leetcode/RemoveLinkedListElements_203.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+public class RemoveLinkedListElements_203 {
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            if (curr.val == val) {
+                ListNode newCurr = deleteNode(prev, curr);
+                if (head == curr) {
+                    head = newCurr;
+                }
+                curr = newCurr;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+
+    private ListNode deleteNode(ListNode prev, ListNode delete) {
+        ListNode next = delete.next;
+        delete.next = null;
+        if (prev != null) {
+            prev.next = next;
+        }
+        return next;
+    }
+
+}
