467be14547190bed1d11c7744ecfe97516bac4e4
Sattar <salambaev.sattar@gmail.com>
2019-05-25 16:10:50 +0600

    poom poom

---
+++ leetcode/IsomorphicString_205.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class IsomorphicString_205 {
+
+    public boolean isIsomorphic(String s, String t) {
+        int[] m = new int[512];
+        for(int i = 0; i < s.length(); i++) {
+            if(m[s.charAt(i)] != m[t.charAt(i)+256]) return false;
+            m[s.charAt(i)] = i + 1;
+            m[t.charAt(i) + 256] = i + 1;
+        }
+        return true;
+    }
+}
