4966e944ff6fdafabc5881947446b8094cd3d700
Sattar <salambaev.sattar@gmail.com>
2019-09-02 14:56:15 +0600

    one more

---
+++ leetcode/easy/IsSubsequence_392.java
@@ -0,0 +1,25 @@
+package leetcode.easy;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsSubsequence_392 {
+
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        if (t.length() == 0) return false;
+        if (s.length() > t.length()) return false;
+        char[] subs = s.toCharArray();
+        char[] itselft = t.toCharArray();
+        int i = 0;
+        for (int j = 0; j < itselft.length; j++) {
+            if (i < subs.length && itselft[j] == subs[i]) {
+                i++;
+            } else if (i == subs.length) return true;
+        }
+        return i == subs.length;
+    }
+
+}
