ccde4fd3313cb43ef0f27f2aa9acb37d65d60e5e
Sattar <salambaev.sattar@gmail.com>
2019-09-03 11:27:53 +0600

    binary watch submitted using hamming weight

---
+++ leetcode/easy/BinaryWatch_401.java
@@ -0,0 +1,30 @@
+package leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryWatch_401 {
+
+    public List<String> readBinaryWatch(int num) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            for (int k = 0; k < 60; k++) {
+                if (hammingWeight(i) + hammingWeight(k) == num) {
+                    result.add(String.format("%d:%02d", i, k));
+                }
+            }
+        }
+        return result;
+    }
+
+    private int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
