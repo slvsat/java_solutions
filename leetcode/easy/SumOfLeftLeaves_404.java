50b0e90036392676b6ea6a108de4297c3cf97775
Sattar <salambaev.sattar@gmail.com>
2019-09-03 12:19:21 +0600

    Sum of Left Leaves through recursion

---
+++ leetcode/easy/SumOfLeftLeaves_404.java
@@ -0,0 +1,26 @@
+package leetcode.easy;
+
+import leetcode.structures.TreeNode;
+import sun.reflect.generics.tree.Tree;
+
+public class SumOfLeftLeaves_404 {
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 0;
+        return getVal(root.left, true) + getVal(root.right, false);
+    }
+
+    private int getVal(TreeNode root, Boolean leftLeaf) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return leftLeaf ? root.val : 0;
+        if (root.left == null && root.right != null) {
+            return getVal(root.right, false);
+        }
+        if (root.right == null && root.left != null) {
+            return getVal(root.left, true);
+        }
+        return getVal(root.left, true) + getVal(root.right, false);
+    }
+
+}
