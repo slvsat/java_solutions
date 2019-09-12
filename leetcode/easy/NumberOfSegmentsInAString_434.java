a97bc3cc5950cd89cad2cc1fbe38f620958c98cb
Sattar <salambaev.sattar@gmail.com>
2019-09-12 19:00:59 +0600

    Number of segments in a string

---
+++ leetcode/easy/NumberOfSegmentsInAString_434.java
@@ -0,0 +1,16 @@
+package leetcode.easy;
+
+public class NumberOfSegmentsInAString_434 {
+
+    public int countSegments(String s) {
+        if (s.length() < 1) return 0;
+        int segments = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i-1) == ' ') && s.charAt(i) != ' ') {
+                segments++;
+            }
+        }
+        return segments;
+    }
+
+}
