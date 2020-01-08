fce83fadcfd4e0df49d64a4abb4b625ef192f8d3
Sattar Salambayev <ssalambayev@dar.kz>
2020-01-08 15:43:29 +0600

    WIP - updates

---
+++ leetcode/stormik/RemoveElement_27.java
@@ -0,0 +1,29 @@
+package leetcode.stormik;
+
+public class RemoveElement_27 {
+
+    public int removeElement(int[] nums, int val) {
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] == val) {
+                for (int j=i; j<nums.length; j++) {
+                    if (nums[j] != val) {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+        int count = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] != val) {
+                count++;
+                continue;
+            }
+            break;
+        }
+        return count;
+    }
+
+}
